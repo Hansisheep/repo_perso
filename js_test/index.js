7dde10c5eeb53afd30f25a25d0aa1bd27dd658da
Hansisheep <hansisheep@live.fr>
2015-11-27 15:29:51 +0100

    MaJ 27/11

--- js_test/index.js
+++ js_test/index.js
@@ -1,11 +1,38 @@
-function prepa()
+$(window).load(function prepa()
 {
-	var head1 = $("<div>").attr("id", "divheader").text("Test Header").css("text-align", "center");
-	var sep = $("<hr>");
-	var div1 = $("<section>").text("Test Div");
-	var footer1 = $("<footer>").text("Test Footer");
-	var button1 = $("<button>").attr("id", "test").text("Panel").css("background-color", "rgb(100, 100, 100)").css("border", "none").css("height", "10%").css("width", "25%");
+	$("body").css("background-image", "url('style/1339717-1920x1080.jpg')").css("margin", "7%");
 
-	$("body").css("margin", "0%").append(div1).append(footer1);
-	$("head").css("margin", "0%").css("display", "inline").append(head1).append(button1).append(sep);
+head();
+body();
+footer();
+});
+
+function head()
+{
+var button1 = $("<button>").attr("id", "test").text(buttonPr).css("background-color", "rgb(100,100,100)").css("height", "10%").css("width", "25%").css("display", "inline");
+var button2 = $("<button>").attr("id", "test").text(buttonSc).css("background-color", "rgb(100,100,100)").css("height", "10%").css("width", "25%").css("display", "inline");
+var button3 = $("<button>").attr("id", "test").text(buttontr).css("background-color", "rgb(100,100,100)").css("height", "10%").css("width", "25%").css("display", "inline");
+var button4 = $("<button>").attr("id", "test").text(buttonqu).css("background-color", "rgb(100,100,100)").css("height", "10%").css("width", "25%").css("display", "inline");
+
+$("head").css("display", "inline").append(button1).append(button2).append(button3).append(button4);
+}
+
+function body()
+{
+var buttonacon = $("<button>").attr("id", "test").text("A faire bouton a con qui compte le nombre de clic").css("background-color", "rgb(100,100,100)").css("height", "10%").css("width", "25%").css("display", "inline");
+
+var section1 = $("<section>").attr("id", "sectionpr");
+var divsection1 = $("<div>").attr("id", "divpr").css("background-color", "rgb(150,150,150)").css("border-style", "outset");
+var para1 = $("<p>").text(textdivpr).attr("margin", "15%").css("text-align", "center");
+var h1= $("<h1>").text(titredivpr);
+
+$("body").append(section1);
+$(section1).append(divsection1);
+$(divsection1).append(para1);
+$(para1).prepend(h1);
+}
+
+function footer()
+{
+
 }
---
+++ js_test/utilitaire/alltext.js
@@ -0,0 +1,3 @@
+var buttonPr = "Principal"; var buttonSc = "Secondaire"; var buttontr = "Recherche"; var buttonqu = "Inscription";
+
+var titredivpr = "Bienvenue sur Grey Chameleon !"; var textdivpr = "";
